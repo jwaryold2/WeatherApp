2a498c16eb99e0c390e5e1edb1dea033a58b91cd
jwaryold2 <71293360+jwaryold2@users.noreply.github.com>
2022-06-18 14:19:27 -0400

    Create WeatherResult.java

---
+++ WeatherResult.java
@@ -0,0 +1,10 @@
+package application;
+
+public class WeatherResult {
+	public String main;
+	public String description;
+	public double temp;
+	public double feels_like;
+	public double temp_min;
+	public double temp_max;
+}
